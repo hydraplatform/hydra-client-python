9397eaa353bb3d8ed9231bee03e38f42a23b27db
James Tomlinson <tomo.bbe@gmail.com>
2018-04-23 08:45:31 +0100

    Completed setup.py
    (cherry picked from commit 8cbff529f3605c67998f0103179d80f510bbf538)

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hydra-client-python',
+    version='0.1',
+    description='Client side libraries for Hydra.',
+    author='Stephen Knox',
+    author_email='stephen.knox@manchester.ac.uk',
+    url='https://github.com/hydraplatform/hydra-client-python',
+    packages=find_packages(),
+    install_requires=['lxml', 'hydra_base', 'requests'],
+)
