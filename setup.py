166154f91949d230c742a094aba135710986d066
knox <stephen.knox@manchester.ac.uk>
2019-11-18 15:01:35 +0000

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hydra-client-python',
-    version='0.1',
+    version='0.1.1',
     description='Client side libraries for Hydra.',
     author='Stephen Knox',
     author_email='stephen.knox@manchester.ac.uk',
