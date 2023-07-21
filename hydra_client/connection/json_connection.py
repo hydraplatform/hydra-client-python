59c37e8f774a1071f08f0df60cd3e138272943c3
Stephen Knox <stephen.knox@manchester.ac.uk>
2023-07-21 16:16:24 +0100

    Correctly close db connections after functions

--- hydra_client/connection/json_connection.py
+++ hydra_client/connection/json_connection.py
@@ -71,6 +71,10 @@ class JSONConnection(BaseConnection):
             hb.db.DBSession.rollback()
             hb.rollback_transaction()
             raise
+        finally:
+            hb.db.close_session()
+            hb.db.engine.dispose()
+
 
         # Call the HB function
 
